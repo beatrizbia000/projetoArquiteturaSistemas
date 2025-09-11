7130387a88fa833c00b05f3a8f42a0e3050e906e
Ana Beatriz Sousa <abeatrizsousadelima@gmail.com>
2025-09-11 19:48:36 -0300

    questao 2 atividade 03

---
+++ Clt.java
@@ -0,0 +1,20 @@
+
+public class Clt implements Pagamento {
+    private float salarioBase;
+    private float beneficios; // ex.: vale-refeição, plano de saúde etc.
+
+    public Clt(float salarioBase, float beneficios) {
+        this.salarioBase = salarioBase;
+        this.beneficios  = beneficios;
+    }
+
+    // regra de cálculo específica para CLT
+    private float salarioComBeneficios() {
+        return salarioBase + beneficios;
+    }
+
+    @Override
+    public float getSaldo() {
+        return salarioComBeneficios();
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+
 public class Main {
     public static void main(String[] args) {
         Pagamento contrato = new Contrato(5000f);
         Pagamento estagio  = new Estagio(1500f);
+        Pagamento clt      = new Clt(4000f, 800f); // novo colaborador CLT
 
         Folha folha = new Folha();
 
@@ -10,5 +12,8 @@ public class Main {
 
         folha.calcular(estagio);
         System.out.println("Bolsa Estágio: R$ " + folha.getSaldo());
+
+        folha.calcular(clt);
+        System.out.println("Salário CLT: R$ " + folha.getSaldo());
     }
 }
